6a260ad18c93f91988313ef256c06849a27cdebe
Airtnp <xiaolr2003@foxmail.com>
2017-05-14 00:01:02 +0800

    Rearrange haskell file place

---
+++ Idioms/call-cc.hs
@@ -0,0 +1,2 @@
+-- ref : http://stackoverflow.com/questions/9050725/call-cc-implementation
+--     : https://zhihu.com/question/40806617/answer/89384029
---
+++ Idioms/free.hs
@@ -0,0 +1,1 @@
+-- TODO: free Monad/free Applicative
