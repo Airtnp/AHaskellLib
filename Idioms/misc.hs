0718fd4df6d33a70b9b4d06c5e98c988387097b4
Airtnp <xiaolr2003@foxmail.com>
2017-05-18 00:12:19 +0800

    Add base cases

--- Idioms/misc.hs
+++ Idioms/misc.hs
@@ -1,5 +1,25 @@
+-- Misc
+
 -- Almost Haskell
 -- ref: https://wiki.haskell.org/Almost_Haskell
 
 -- Perfunctors are like functors, but more perfunctory. We have:
 prefunctory :: (Perfunctor (f :: * -> *)) => f a -> f a
+
+-- Base cases and identities
+-- ref: https://wiki.haskell.org/Base_cases_and_identities
+
+-- To determine base cases
+sum [] = {- How to determine? -} 0
+sum (x:xs) = x + sum xs
+
+{-
+    Pred:
+        sum [x] == x
+            sum xs + sum ys == sum (xs ++ ys)
+
+    Deduct: 
+        sum [] + sum [0] == sum ([] ++ [0])
+            => sum [] + 0 == 0
+            => sum [] == 0
+-}
