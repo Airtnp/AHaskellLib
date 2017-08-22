3700a4ac85acde3b8b6dcf11ff8c879d42a91306
Airtnp <xiaolr2003@foxmail.com>
2017-08-23 01:15:18 +0800

    add Y

---
+++ Utils/Y.hs
@@ -0,0 +1,40 @@
+module Y where
+
+newtype Rec a = Rec { recOut :: Rec a -> a }
+
+y :: (a -> a) -> a
+y = \f ->
+        (\x -> 
+            f (recOut x $ x)
+        )
+        (
+            Rec (
+                \x ->
+                    f (recOut x $ x)
+            )
+        )
+
+#ifndef __GLASGOW_HASKELL__
+
+fix f = y f
+
+#else
+
+fix f = fixH (Rec fixH)
+    where
+        {-# NOINLINE fixH #-}
+        fixH x = f (recOut x $ x)
+
+#endif
+
+newtype RecM a = RecM (RecM a -> a)
+y2 = \f ->
+        (\h -> 
+            h $ RecM h
+        )
+        (
+            \x ->
+                f . (\(RecM g) ->
+                    g    
+                ) x $ x
+        )
